c4477a2949706a2be135ed14533a59a79b8c3676
RickArko <rickarko@pm.me>
2026-07-11 15:13:52 -0500

    configure chains in dbt

--- dbt/macros/normalize_wallet_address.sql
+++ dbt/macros/normalize_wallet_address.sql
@@ -1,6 +1,10 @@
 {% macro normalize_wallet_address(address_column, chain_column) %}
   case
-    when {{ chain_column }} in ('arbitrum', 'ethereum') then lower({{ address_column }})
+    when {{ chain_column }} in (
+      {%- for chain in var('evm_chains') -%}
+        '{{ chain }}'{% if not loop.last %}, {% endif %}
+      {%- endfor -%}
+    ) then lower({{ address_column }})
     else {{ address_column }}
   end
 {% endmacro %}
